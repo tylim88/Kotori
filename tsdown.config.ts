f8e10f9339d3c5d75bee3426cf3bba6b24f062ea
tylim <limteikyee@gmail.com>
2026-04-24 16:45:12 +0800

    disable minification

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
 	entry: ['src/index.ts'],
 	clean: true,
-	minify: !false,
+	minify: false,
 	format: ['cjs', 'esm'],
 	outDir: 'dist',
 	dts: true,
